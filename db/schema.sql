045be786d5110e024c0d30534cc8b95a2b586224
EC2 Default User <ec2-user@ip-172-31-43-49.us-east-2.compute.internal>
2025-06-09 21:27:28 +0000

    updated case for table fields and foreign key relation for measurements and sensors

--- db/schema.sql
+++ db/schema.sql
@@ -2,13 +2,13 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS countries (
 	id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-	api_countryId INTEGER NOT NULL,
+	api_countryid INTEGER NOT NULL,
 	name TEXT
 	);
 
 CREATE TABLE IF NOT EXISTS parameters (
 	id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-	api_parameterId INTEGER NOT NULL,
+	api_parameterid INTEGER NOT NULL,
 	units TEXT,
 	name TEXT,
 	description TEXT
@@ -16,21 +16,21 @@ CREATE TABLE IF NOT EXISTS parameters (
 
 CREATE TABLE IF NOT EXISTS locations (
 	id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-	api_locationId INTEGER NOT NULL,
+	api_locationid INTEGER NOT NULL,
 	name TEXT,
 	city TEXT,
 	latitude DOUBLE PRECISION,
 	longitude DOUBLE PRECISION,
-	countryId UUID,
+	countryid UUID,
 	FOREIGN KEY (countryId) REFERENCES countries(id)
 	);
 
 CREATE TABLE IF NOT EXISTS sensors (
 	id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-	api_sensorId INTEGER NOT NULL,
+	api_sensorid INTEGER UNIQUE NOT NULL,
 	name TEXT,
-	locationId UUID,
-	parameterId UUID,
+	locationid UUID,
+	parameterid UUID,
 	FOREIGN KEY (locationId) REFERENCES locations(id),
 	FOREIGN KEY (parameterId) REFERENCES parameters(id)
 	);
@@ -39,6 +39,6 @@ CREATE TABLE IF NOT EXISTS measurements (
 	id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
 	datetime TIMESTAMP NOT NULL,
 	value DOUBLE PRECISION NOT NULL,
-	sensorId UUID,
-	FOREIGN KEY (sensorId) REFERENCES sensors(id)
+	api_sensorid INTEGER NOT NULL,
+	FOREIGN KEY (api_sensorid) REFERENCES sensors(api_sensorid)
 	);
