6910d7c342910b4283e1b20c0059c77aa61dee28
EC2 Default User <ec2-user@ip-172-31-43-49.us-east-2.compute.internal>
2025-06-13 21:53:11 +0000

    Created transform_country.py

---
+++ etl/transform/transform_country.py
@@ -0,0 +1,66 @@
+import argparse
+import pandas as pd
+from pathlib import Path
+import os
+from dotenv import load_dotenv
+import logging
+import json
+
+
+load_dotenv()
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s: %(message)s")
+
+DATA_DIR = Path(os.getenv("DATA_DIR"))
+RAW_DATA_DIR = DATA_DIR / "raw"
+RAW_DATA_DIR.mkdir(parents=True, exist_ok=True)
+CLEAN_DATA_DIR = DATA_DIR / "clean"
+CLEAN_DATA_DIR.mkdir(parents=True, exist_ok=True)
+
+def transform_country(filename: str):
+    """
+    Transforms the raw json file containing a country's information into parquet format.
+
+    Args:
+        filename (str): The filename of the raw data json file.
+
+    Raises:
+        ValueError: If the file is not .json, does not contain the string 'country', or contains no records in the results array.
+    """
+
+    if not filename.endswith(".json"):
+        raise ValueError(f"Expected .json file. Got {filename}")
+    if "country" not in filename:
+        raise ValueError(f"Expected filename to contain 'country'. Got {filename}")
+
+    filename = Path(filename).name
+    filepath = RAW_DATA_DIR / filename
+    clean_filepath = CLEAN_DATA_DIR / filename.replace(".json",".parquet")
+
+    with open(filepath, "r") as f:
+        data = json.load(f)
+
+    if len(data.get("results", []))==0:
+        raise ValueError(f"No records present in {filename}")
+
+    logging.info(f"Successfully loaded {filename}")
+
+    records = [
+            {
+                "api_countryid": r["id"],
+                "name": r["name"]
+                } for r in data["results"]
+            ]
+    df = pd.DataFrame(records)
+    df.to_parquet(clean_filepath, engine="pyarrow", index=False)
+    logging.info(f"Saved {len(records)} clean records to {clean_filepath.name}")
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--filename", required=True, help="Filename of the raw json file containing the country's information.")
+    args = parser.parse_args()
+
+    transform_country(args.filename)
+
+if __name__ == "__main__":
+    main()
