d52b3c1698e44e9f69869aa6a2cba6814ba2523a
EC2 Default User <ec2-user@ip-172-31-43-49.us-east-2.compute.internal>
2025-06-14 04:58:51 +0000

    Created transform_location.py

---
+++ etl/transform/transform_location.py
@@ -0,0 +1,65 @@
+import argparse
+from pathlib import Path
+import os
+from dotenv import load_dotenv
+import pandas as pd
+import json
+import logging
+
+load_dotenv()
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s: %(message)s")
+
+DATA_DIR = Path(os.getenv("DATA_DIR"))
+RAW_DATA_DIR = DATA_DIR / "raw"
+RAW_DATA_DIR.mkdir(parents=True, exist_ok=True)
+CLEAN_DATA_DIR = DATA_DIR / "clean"
+CLEAN_DATA_DIR.mkdir(parents=True, exist_ok=True)
+
+def transform_location(filename: str):
+    """
+    Transforms raw json file containing location information to parquet format.
+
+    Args:
+        filename (str): Filename of the raw json data to be cleaned.
+
+    Raises:
+        ValueError: If filename is not .json, filename does not contain 'location', or results array contains 0 records.
+    """
+
+    if not filename.endswith(".json"):
+        raise ValueError(f"Expected json file. Got {filename}")
+    if 'location' not in filename:
+        raise ValueError(f"Expected 'location' to be in filename. Got {filename}")
+
+    filename = Path(filename).name
+    filepath = RAW_DATA_DIR / filename
+    clean_filepath = CLEAN_DATA_DIR / filename.replace(".json", ".parquet")
+
+    with open(filepath, "r") as f:
+        data = json.load(f)
+
+    if len(data.get('results', []))==0:
+        raise ValueError(f"No records found in {filename}")
+
+    records = [{
+        "api_locationid": r["id"],
+        "name": r["name"],
+        "latitude": r["coordinates"]["latitude"],
+        "longitude": r["coordinates"]["longitude"],
+        "api_countryid": r["country"]["id"]
+        } for r in data["results"]]
+
+    df = pd.DataFrame(records)
+    df.to_parquet(clean_filepath, engine="pyarrow", index=False)
+    logging.info(f"Saved {len(df)} clean records to {clean_filepath.name}")
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--filename", required=True, help="Filename of the raw json file containing location information.")
+    args = parser.parse_args()
+
+    transform_location(args.filename)
+
+if __name__ == "__main__":
+    main()
