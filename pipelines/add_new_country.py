e40b4fc794249299170b42408b1fa1715e63737a
EC2 Default User <ec2-user@ip-172-31-43-49.us-east-2.compute.internal>
2025-06-24 21:28:12 +0000

    Created pipeline for adding new country

---
+++ pipelines/add_new_country.py
@@ -0,0 +1,47 @@
+import logging
+from pathlib import Path
+import os
+from etl.ingestion.fetch_country import fetch_country
+from etl.transform.transform_country import transform_country
+from etl.load.load_country import load_country
+from dotenv import load_dotenv
+from db.db import get_db
+import argparse
+
+load_dotenv()
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s: %(message)s")
+
+def add_new_country(country_id: int):
+    
+    db = next(get_db())
+
+    try:
+        logging.info(f"Fetching country information for country {country_id}")
+
+        raw_filepath = fetch_country(country_id)
+    except Exception as e:
+        logging.exception(f"Error while fetching data from OpenAQ API")
+
+    try:
+        clean_filepath = transform_country(raw_filepath)
+    except Exception as e:
+        logging.exception(f"Error while transforming data from {clean_filepath}")
+
+    try:
+        load_country(clean_filepath, db)
+        logging.info(f"Country {country_id} successfully added to database.")
+    except Exception as e:
+        logging.exception(f"Error while inserting data into database for country {country_id}")
+
+    db.close()
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--country", required=True, help="Country id as recognized by the OpenAQ API.")
+    args = parser.parse_args()
+
+    add_new_country(args.country)
+
+if __name__ == "__main__":
+    main()
