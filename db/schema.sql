9ea9f1e19a10e9de85b0b81b069212243e87d1ad
EC2 Default User <ec2-user@ip-172-31-43-49.us-east-2.compute.internal>
2025-06-24 15:51:11 +0000

    Updated db schema to get rid of api IDs and make primary keys integer type rather than uuid

--- db/schema.sql
+++ db/schema.sql
@@ -1,43 +1,37 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
 CREATE TABLE IF NOT EXISTS countries (
-	id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-	api_countryid INTEGER NOT NULL,
+	id INTEGER PRIMARY KEY,
 	name TEXT
 	);
 
 CREATE TABLE IF NOT EXISTS parameters (
-	id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-	api_parameterid INTEGER NOT NULL,
+	id INTEGER PRIMARY KEY,
 	units TEXT,
 	name TEXT,
 	description TEXT
 	);
 
 CREATE TABLE IF NOT EXISTS locations (
-	id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-	api_locationid INTEGER NOT NULL,
+	id INTEGER PRIMARY KEY,
 	name TEXT,
 	latitude DOUBLE PRECISION,
 	longitude DOUBLE PRECISION,
-	api_countryid UUID,
-	FOREIGN KEY (api_countryId) REFERENCES countries(api_countryid)
+	country_id INTEGER,
+	FOREIGN KEY (country_id) REFERENCES countries(id)
 	);
 
 CREATE TABLE IF NOT EXISTS sensors (
-	id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-	api_sensorid INTEGER UNIQUE NOT NULL,
+	id INTEGER PRIMARY KEY,
 	name TEXT,
-	api_locationid UUID,
-	api_parameterid UUID,
-	FOREIGN KEY (api_locationId) REFERENCES locations(api_locationid),
-	FOREIGN KEY (api_parameterId) REFERENCES parameters(api_parameterid)
+	location_id INTEGER,
+	parameter_id INTEGER,
+	FOREIGN KEY (location_id) REFERENCES locations(id),
+	FOREIGN KEY (parameter_id) REFERENCES parameters(id)
 	);
 
 CREATE TABLE IF NOT EXISTS measurements (
-	id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+	id INTEGER PRIMARY KEY,
 	datetime TIMESTAMP NOT NULL,
 	value DOUBLE PRECISION NOT NULL,
-	api_sensorid INTEGER NOT NULL,
-	FOREIGN KEY (api_sensorid) REFERENCES sensors(api_sensorid)
+	sensor_id INTEGER NOT NULL,
+	FOREIGN KEY (sensor_id) REFERENCES sensors(id)
 	);
