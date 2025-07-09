5a8754bb306e4a3370e7da41eeec5cf78a4504db
EC2 Default User <ec2-user@ip-172-31-41-25.us-east-2.compute.internal>
2025-07-09 19:39:31 +0000

    Fixed environment variable passing in DockerOperator

--- airflow/dags/hourly_ingestion.py
+++ airflow/dags/hourly_ingestion.py
@@ -1,6 +1,7 @@
 from airflow import DAG
 from airflow.providers.docker.operators.docker import DockerOperator
 from datetime import datetime, timedelta
+from dotenv import dotenv_values
 
 default_args = {
         "retries": 1,
@@ -8,4 +9,4 @@ default_args = {
         }
 
 with DAG(dag_id="hourly_ingestion", start_date=datetime(2023, 1, 1), schedule_interval="@hourly", catchup=False, default_args=default_args) as dag:
-    task = DockerOperator(task_id="run_hourly_ingestion", image="hourly_ingestion", command="python run.py", network_mode="bridge", auto_remove="force", volumes=["/opt/airflow/.env:/usr/local/app/.env"])
+    task = DockerOperator(task_id="run_hourly_ingestion", image="hourly_ingestion", command="python run.py", environment=dotenv_values("../.env"), network_mode="bridge", auto_remove="never")
