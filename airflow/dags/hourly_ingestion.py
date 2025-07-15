0a3cb6bddfb57bd5da1c0d79637ad1383f09d654
EC2 Default User <ec2-user@ip-172-31-41-25.us-east-2.compute.internal>
2025-07-15 15:52:34 +0000

    Committing these unfinished changes to docker/airflow workflow so that I can move on with the API

--- airflow/dags/hourly_ingestion.py
+++ airflow/dags/hourly_ingestion.py
@@ -1,12 +1,23 @@
 from airflow import DAG
 from airflow.providers.docker.operators.docker import DockerOperator
 from datetime import datetime, timedelta
-from dotenv import dotenv_values
+import os
 
 default_args = {
         "retries": 1,
         "retry_delay": timedelta(minutes=5)
         }
 
+env_vars = {
+        "DB_USER": os.environ["DB_USER"],
+        "DB_NAME": os.environ["DB_NAME"],
+        "DB_PORT": os.environ["DB_PORT"],
+        "DB_HOST": os.environ["DB_HOST"],
+        "DB_PASSWORD": os.environ["DB_PASSWORD"],
+        "OPENAQ_API_BASE": os.environ["OPENAQ_API_BASE"],
+        "DATA_DIR": os.environ["DATA_DIR"],
+        "API_KEY": os.environ["API_KEY"]
+        }
+
 with DAG(dag_id="hourly_ingestion", start_date=datetime(2023, 1, 1), schedule_interval="@hourly", catchup=False, default_args=default_args) as dag:
-    task = DockerOperator(task_id="run_hourly_ingestion", image="hourly_ingestion", command="python run.py", environment=dotenv_values("../.env"), network_mode="bridge", auto_remove="never")
+    task = DockerOperator(task_id="run_hourly_ingestion", image="hourly_ingestion", command="python run.py", environment=env_vars, network_mode="bridge", auto_remove="never")
